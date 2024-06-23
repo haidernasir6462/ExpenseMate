c562c0146f37d9f4e7a3df652c704197f843c261
Haider Nasir <haider.nasir6462@gmail.com>
2024-06-23 18:59:20 +0000

    implement desktop navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,16 @@
 import { UserButton } from "@clerk/nextjs";
-import HeaderLogo from "./HeaderLogo";
+import HeaderLogo from "@/components/HeaderLogo";
+import Navigation from "@/components/Navigation";
 export default function Header() {
   return (
     <header className="bg-gradient-to-b from-blue-600 to-blue-400 px-4 lg:px-14 py-8 pb-36">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between">
-          <div className="flex itemsc-center justify-center gap-x-16">
-          <HeaderLogo />
-          <p>logo</p>
-          <p>nav</p>
-          {/* <Nav /> */}
+          <div className="hidden lg:flex itemsc-center justify-center gap-x-16">
+            <HeaderLogo />
+            <Navigation />
           </div>
-        <UserButton afterSignOutUrl="/" />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </header>
--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function HeaderLogo() {
   return (
-    <Link href="/" className="flex items-center justify-center gap-x-4">
+    <Link href="/" className="hidden lg:flex items-center justify-center gap-x-4">
       <Image src="/logo.svg" alt="logo"  width={28} height={28} className="text-white"/>
       <h1 className="text-white text-2xl font-semibold">ExpenseMate</h1>
     </Link>
