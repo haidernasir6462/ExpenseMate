c2010dabde98fc9c0f3d6241ec0f4cc1729d44e5
Haider Nasir <haider.nasir6462@gmail.com>
2024-06-29 19:14:21 +0000

    fetch data from account schema in database

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,17 @@
+import { db } from "@/db/drizzle";
+import { accounts } from "@/db/schema";
 import { Hono } from "hono";
 
-const app = new Hono().get("/", (c) => {
+const app = new Hono().get("/", async (c) => {
+  const data = await db
+    .select({
+      id: accounts.id,
+      name: accounts.name,
+    })
+    .from(accounts);
+
   return c.json({
-    accounts: [],
+    data,
   });
 });
 
