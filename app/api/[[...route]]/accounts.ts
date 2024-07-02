6698982b16cbe36b76b9897ec153a83243e56987
Haider Nasir <haider.nasir6462@gmail.com>
2024-07-02 18:07:51 +0000

    protect the getAccount API on server side

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,18 +1,29 @@
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { Hono } from "hono";
 
-const app = new Hono().get("/", async (c) => {
-  const data = await db
-    .select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts);
+const app = new Hono().get(
+  "/",
+  clerkMiddleware(),
+  async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json({ error: "unauthorized" }, 401);
+    }
+  },
+  async (c) => {
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts);
 
-  return c.json({
-    data,
-  });
-});
+    return c.json({
+      data,
+    });
+  }
+);
 
 export default app;
