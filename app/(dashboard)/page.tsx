feb274115cfd25c9a0c37d8e621b1bc6ff4f3a24
Haider Nasir <haider.nasir6462@gmail.com>
2024-07-06 17:17:24 +0000

    implement global-add-transaction modal using zustand

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
 import React from "react";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
   const { data: accounts, isLoading, error } = useGetAccounts();
-  console.log("🚀 ~ Home ~ error:", error)
-  console.log("🚀 ~ Home ~ accounts:", accounts)
+  const { onOpen } = useNewAccount();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -16,16 +17,19 @@ export default function Home() {
   }
 
   return (
-    <div>
-      {accounts?.length ? (
-        accounts.map((account) => (
-          <div key={account.id}>
-            <p>{account.name}</p>
-          </div>
-        ))
-      ) : (
-        <div>No accounts found.</div>
-      )}
-    </div>
+    <>
+      <div>
+        {accounts?.length ? (
+          accounts.map((account) => (
+            <div key={account.id}>
+              <p>{account.name}</p>
+            </div>
+          ))
+        ) : (
+          <div>No accounts found.</div>
+        )}
+      </div>
+      <Button onClick={onOpen}> Create a new transaction</Button>
+    </>
   );
 }
