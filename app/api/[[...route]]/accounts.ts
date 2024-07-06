9816912b829de1954493307144bc986965c681a8
Haider Nasir <haider.nasir6462@gmail.com>
2024-07-06 10:12:33 +0000

    format the API

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -4,8 +4,8 @@ import { auth } from "@clerk/nextjs/server";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { zValidator } from '@hono/zod-validator'
-import {createId} from "@paralleldrive/cuid2"
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
 
 const app = new Hono()
   .get(
@@ -24,7 +24,7 @@ const app = new Hono()
           name: accounts.name,
         })
         .from(accounts);
-      // .where(eq(accounts.userId, auth.userId));
+      // .where(eq(accounts.userId, auth?.userId));
 
       return c.json({
         data,
@@ -33,23 +33,29 @@ const app = new Hono()
   )
   .post(
     "/",
-    zValidator("json", insertAccountSchema.pick({
-      name: true
-    })),
+    zValidator(
+      "json",
+      insertAccountSchema.pick({
+        name: true,
+      })
+    ),
     clerkMiddleware(),
     async (c) => {
-      const values= c.req.valid("json");
+      const values = c.req.valid("json");
       const auth = getAuth(c);
       if (!auth?.userId) {
         return c.json({ error: "unauthorized" }, 401);
       }
 
-      const [data] = await db.insert(accounts).values({
-        id : createId(),
-        userId: auth.userId,
-        ...values,
-      }).returning()
-      return c.json({data})
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+      return c.json({ data });
     }
   );
 
