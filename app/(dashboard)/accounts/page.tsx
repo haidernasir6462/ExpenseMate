4ca7a9eea33ae0dd5f23b27d85a7c2e6fa7ad1bd
Haider Nasir <haider.nasir6462@gmail.com>
2024-07-20 13:54:26 +0100

    implement account page with data table using dummy data

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/data-table";
 import {
   Card,
   CardHeader,
@@ -10,7 +11,9 @@ import {
   CardDescription,
   CardTitle,
 } from "@/components/ui/card";
+import { Payment, columns } from "./columns";
 import { Plus } from "lucide-react";
+import { Row } from "@tanstack/react-table";
 
 export default function AccountPage() {
   // const { data: accounts, isLoading, error } = useGetAccounts();
@@ -24,19 +27,41 @@ export default function AccountPage() {
   //   return <div>Error fetching accounts: {error.message}</div>;
   // }
 
+  const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 200,
+      status: "success",
+      email: "haider@example.com",
+    },
+  ];
   return (
     <>
       <div className="mx-auto max-w-screen-2xl w-full pb-10 -mt-24">
-        
-      <Card className="border-none drop-shadow-none">
-        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-          <CardTitle className="text-xl line-clamp-1">Account</CardTitle>
-          <Button size="sm" onClick={onOpen}>
-            <Plus className="size- mr-2"/>
-            Add new
-          </Button>
-        </CardHeader>
-      </Card>
+        <Card className="border-none drop-shadow-none">
+          <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+            <CardTitle className="text-xl line-clamp-1">Account</CardTitle>
+            <Button size="sm" onClick={onOpen}>
+              <Plus className="size- mr-2" />
+              Add new
+            </Button>
+          </CardHeader>
+          <CardContent>
+            <DataTable
+              columns={columns}
+              data={data}
+              filterKey="email"
+              onDelete={() => {}}
+              disabled
+            />
+          </CardContent>
+        </Card>
       </div>
     </>
   );
