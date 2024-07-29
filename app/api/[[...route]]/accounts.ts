7e290c607b8c5aca44e039e2a1c074cc8a51be3b
Haider Nasir <haider.nasir6462@gmail.com>
2024-07-29 13:07:21 +0100

    testing commit #implement swaggger ui

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -6,26 +6,32 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
 import { z } from "zod";
+import { swaggerUI } from "@hono/swagger-ui";
 
 const app = new Hono()
-  .get("/", clerkMiddleware(), async (c) => {
-    const auth = getAuth(c);
-    if (!auth?.userId) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
-    // using drizzle ORM structure
-    const data = await db
-      .select({
-        id: accounts.id,
-        name: accounts.name,
-      })
-      .from(accounts)
-      .where(eq(accounts.userId, auth?.userId));
+  .get(
+    "/",
+    swaggerUI({ url: "/https://expense-mate-amber.vercel.app/" }),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c);
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+      // using drizzle ORM structure
+      const data = await db
+        .select({
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(eq(accounts.userId, auth?.userId));
 
-    return c.json({
-      data,
-    });
-  })
+      return c.json({
+        data,
+      });
+    }
+  )
   .post(
     "/",
     clerkMiddleware(),
