914f70f25de913a5b5a900cb15da0963707b7d92
Haider Nasir <haider.nasir6462@gmail.com>
2024-07-30 13:07:56 +0100

    create API for add transactions

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -1,26 +1,58 @@
 import { db } from "@/db/drizzle";
-import { transactions } from "@/db/schema";
+import { transactions, insertTransactionSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
+import { z } from "zod";
 
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json({ error: "Unauthorized" }, 401);
-  }
-  // using drizzle ORM structure
-  const data = await db
-    .select({
-      id: transactions.id,
-      name: transactions.name,
-    })
-    .from(transactions)
-    .where(eq(transactions.userId, auth?.userId));
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+    // using drizzle ORM structure
+    const data = await db
+      .select({
+        id: transactions.id,
+        name: transactions.name,
+      })
+      .from(transactions)
+      .where(eq(transactions.userId, auth?.userId));
 
-  return c.json({
-    data,
-  });
-});
+    return c.json({
+      data,
+    });
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      insertTransactionSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const values = c.req.valid("json");
+
+      const auth = getAuth(c);
+      if (!auth?.userId) {
+        return c.json({ error: "unauthorized" }, 401);
+      }
+      // using drizzle ORM
+      const [data] = await db
+        .insert(transactions)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+      return c.json({ data });
+    }
+  );
 
 export default app;
