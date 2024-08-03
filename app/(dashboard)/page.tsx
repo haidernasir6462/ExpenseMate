6735c85f99e9333ad00675ec71a2afe515b7aaf6
Haider Nasir <haider.nasir6462@gmail.com>
2024-08-03 12:26:00 +0100

    revert the commit # implement basic transaction page #commit 5170fb110adb7a0305103b6d9b62999606d65159

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,35 +1,11 @@
 "use client";
 import React from "react";
 
-import { Button } from "@/components/ui/button";
-import { DataTable } from "@/components/data-table";
-import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
-
-import { Plus } from "lucide-react";
-import { columns } from "./accounts/columns";
-
-export default function TransactionPage() {
+export default function Home() {
   return (
     <>
-      <div className="mx-auto max-w-screen-2xl w-full pb-10 -mt-24">
-        <Card className="border-none drop-shadow-none">
-          <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-            <CardTitle className="text-xl line-clamp-1">Account</CardTitle>
-            <Button size="sm">
-              <Plus className="size- mr-2" />
-              Add new
-            </Button>
-          </CardHeader>
-          <CardContent>
-            <DataTable
-              columns={columns}
-              data={[]}
-              filterKey="name"
-              onDelete={() => {}}
-              disabled={true}
-            />
-          </CardContent>
-        </Card>
+      <div>
+        Dashboard page
       </div>
     </>
   );
