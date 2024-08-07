cff56cd1c0282d0744013c1b07c417071d330ffa
Haider Nasir <haider.nasir6462@gmail.com>
2024-08-07 13:57:30 +0100

    create post categories APIs

--- app/api/[[...route]]/categories.ts
+++ app/api/[[...route]]/categories.ts
@@ -1,8 +1,10 @@
 import { db } from "@/db/drizzle";
-import { categories } from "@/db/schema";
+import { categories, insertCategorySchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
 
 const app = new Hono()
   .get("/", clerkMiddleware(), async (c) => {
@@ -23,5 +25,34 @@ const app = new Hono()
       data,
     });
   })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      insertCategorySchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const values = c.req.valid("json");
+
+      const auth = getAuth(c);
+      if (!auth?.userId) {
+        return c.json({ error: "unauthorized" }, 401);
+      }
+      // using drizzle ORM
+      const [data] = await db
+        .insert(categories)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+      return c.json({ data });
+    }
+  )
+  
 
 export default app;
