04f322d160323494442206311b5d86dd867fd199
Haider Nasir <haider.nasir6462@gmail.com>
2024-08-18 13:43:01 +0100

    update the transaction schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,25 +1,35 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
-import { createInsertSchema} from "drizzle-zod"
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
-    id: text("id").primaryKey(),
-    plaidId: text("plaid_id"),
-    name: text("name").notNull(),
-    userId: text("user_id").notNull()
-})
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+});
 
 export const categories = pgTable("categories", {
-    id: text("id").primaryKey(),
-    plaidId: text("plaid_id"),
-    name: text("name").notNull(),
-    userId: text("user_id").notNull()
-})
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+});
 
 export const transactions = pgTable("transactions", {
-    id: text("id").primaryKey(),
-    amount: integer("amount").notNull()
-})
+  id: text("id").primaryKey(),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id").references(() => accounts.id, {
+    onDelete: "cascade",
+  }).notNull(),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }) 
+});
 
 // Schema for inserting a account - can be used to validate API requests
-export const insertAccountSchema = createInsertSchema(accounts) 
-export const insertCategorySchema = createInsertSchema(categories) 
+export const insertAccountSchema = createInsertSchema(accounts);
+export const insertCategorySchema = createInsertSchema(categories);
+export const insertTransactionSchema = createInsertSchema(transactions);
